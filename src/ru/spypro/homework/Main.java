c6ae383f8aa9f1badf625527a0ada43d1fecca5f
dispersion <anyta1901@gmail.com>
2021-12-30 11:09:31 +0300

    Коммит во второй ветке с выполненными заданиями (поправки)

--- src/ru/spypro/homework/Main.java
+++ src/ru/spypro/homework/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-       // exerciseOne();
-       // exerciseTwo();
+        exerciseOne();
+       exerciseTwo();
         exerciseThree();
-       // exerciseFour();
-       // exerciseFive();
+        exerciseFour();
+        exerciseFive();
 
     }
 
@@ -26,46 +26,49 @@ public class Main {
     }
 
     public static void exerciseTwo() {
-        int clientOS = 0;
-        int clientDeviceYear = 2020;
+        int clientOS = 1;
+        int clientDeviceYear = 2000;
 
-        if (clientOS == 0 & clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        if (clientOS == 0 ) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015 ) {
+                System.out.println( "Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
         }
-        else if (clientOS == 0 & clientDeviceYear >= 2015){
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        else if (clientOS == 1 & clientDeviceYear < 2015) {
-            System.out.println( "Установите облегченную версию приложения для Android по ссылке");
-        }
-        else  if (clientOS == 1 & clientDeviceYear >= 2015){
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } }
+
+    }
 
     public static void exerciseThree() {
-        int year = 1900;
-        if ( year % 4 == 0  || year % 400 == 0) {
+        int year = 1700;
+        if ( (year % 4 == 0)  & (year % 400 == 0)) {
             System.out.println( year + " год является високосным");
         }
         else if ( year % 100 == 0) {
             System.out.println( year + " год не является високосным");
         }
-        else {
-        System.out.println( year + " год не является високосным"); }
+        else  {
+            System.out.println( year + " год не является високосным"); }
     }
 
     public static void exerciseFour() {
         int deliveryDistance = 95;
         int deliveryTime = 1;
-       // int deliveryTimes = deliveryTime + 1;
 
-        if (deliveryDistance >= 20 ) {
+
+        if (deliveryDistance <= 20 ) {
             System.out.println("Потребуется дней: " + deliveryTime );
         }
-        else if ( deliveryDistance > 20 || deliveryDistance < 60) {
+        else if ( deliveryDistance > 20 && deliveryDistance <= 60) {
             System.out.println("Потребуется дней: " + (deliveryTime + 1));
         }
-        else if (deliveryDistance > 60 || deliveryDistance <= 100) {
+        else if (deliveryDistance > 60 && deliveryDistance <= 100) {
             System.out.println("Потребуется дней: " + (deliveryTime + 2));
         }
 
@@ -76,37 +79,37 @@ public class Main {
         switch (monthNumber){
             case 1:
                 System.out.println("Январь");
-                break;
+
             case 2:
                 System.out.println("Февраль");
-                break;
+
             case 3:
                 System.out.println("Март");
-                break;
+
             case 4:
                 System.out.println("Апрель");
-                break;
+
             case 5:
                 System.out.println("Май");
-                break;
+
             case 6:
                 System.out.println("Июнь");
-                break;
+
             case 7:
                 System.out.println("Июль");
-                break;
+
             case 8:
                 System.out.println("Август");
-                break;
+
             case 9:
                 System.out.println("Сентябрь");
-                break;
+
             case 10:
                 System.out.println("Октябрь");
-                break;
+
             case 11:
                 System.out.println("Ноябрь");
-                break;
+
             case 12:
                 System.out.println("Декабрь");
                 break;
@@ -115,6 +118,7 @@ public class Main {
         }
 
     }
+
 
 }
 
